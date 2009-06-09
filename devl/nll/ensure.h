3b6bc7c35f8e6b97455ea86022c7228b77b2c67a
civodlu@gmail.com <civodlu@gmail.com@355e9548-2d90-11de-ba44-d58fe8a9ce33>
2009-06-09 09:26:04 +0000

    ensure compilation flag

--- devl/nll/ensure.h
+++ devl/nll/ensure.h
@@ -1,11 +1,21 @@
 #ifndef NLL_ENSURE_H_
 # define NLL_ENSURE_H_
 
+//#define NLL_NO_ENSURE
+
 /**
  * @brief Ensure that the expression is true, else log the error and stop the program
+          it can be turned off by setting the flag <code>NLL_NO_ENSURE</code>
+
+   It is typically used to ensure that the program stays in a correct state for a developer version.
+   However it should be disabled for a released version. It is different from <code>assert</code> since
+   it won't really slow down the components as it is not used in internal loops/critical code.
  */
-# ifdef _MSC_VER
-#  define ensure( _Expression, _String )							      \
+# ifdef NLL_NO_ENSURE
+#  define ensure( _Expression, _String )                 (_Expression)                  
+# else
+#  ifdef _MSC_VER
+#   define ensure( _Expression, _String )							      \
       if ( !( _Expression ) )										         \
       {															               \
          nll::core::LoggerNll::write( nll::core::LoggerNll::ERROR, _String );			\
@@ -16,8 +26,8 @@
          _CrtDbgBreak();                                          \
          exit( 1 );                                               \
       }
-# else
-#  define ensure( _Expression, _String )							      \
+#  else
+#   define ensure( _Expression, _String )							      \
       if ( !( _Expression ) )										         \
       {															               \
          nll::core::LoggerNll::write( nll::core::LoggerNll::ERROR, _String );			\
@@ -27,5 +37,6 @@
 	      std::cout << "  Line     : " << __LINE__ << std::endl;	\
          exit( 1 );                                               \
       }
+#  endif
 # endif
 #endif
