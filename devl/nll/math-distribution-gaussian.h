beeaf5ac3a1ad68bb29c962d062f0820d946f7d9
civodlu@gmail.com <civodlu@gmail.com@355e9548-2d90-11de-ba44-d58fe8a9ce33>
2009-06-27 17:51:55 +0000

    integer overflow

--- devl/nll/math-distribution-gaussian.h
+++ devl/nll/math-distribution-gaussian.h
@@ -18,8 +18,8 @@ namespace core
     */
    inline double NLL_API generateGaussianDistribution( const double mean, const double variance )
    {
-      double u1 = ( static_cast<double> ( rand() ) + 1 ) / ( RAND_MAX + 1 );
-      double u2 = ( static_cast<double> ( rand() ) + 1 ) / ( RAND_MAX + 1 );
+      double u1 = ( static_cast<double> ( rand() ) + 1 ) / ( (double)RAND_MAX + 1 );
+      double u2 = ( static_cast<double> ( rand() ) + 1 ) / ( (double)RAND_MAX + 1 );
       assert( -2 * log( u1 ) >= 0 );
       double t1 = sqrt( -2 * log( u1 ) ) * cos( 2 * core::PI * u2 );
 
