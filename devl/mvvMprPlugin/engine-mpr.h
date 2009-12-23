af12534a04a2b2246150eff2b710f55ca0737715
civodlu@gmail.com <civodlu@gmail.com@355e9548-2d90-11de-ba44-d58fe8a9ce33>
2009-12-23 17:31:40 +0000

    up

--- devl/mvvMprPlugin/engine-mpr.h
+++ devl/mvvMprPlugin/engine-mpr.h
@@ -9,16 +9,20 @@ namespace mvv
 {
 namespace platform
 {
+   namespace impl
+   {
+   }
+
    class EngineMpr : EngineOrder
    {
    public:
-      // public slots
-      ResourceVector3f     position;
-      ResourceVector3f     directionx;
-      ResourceVector3f     directiony;
-      ResourceVector3f     panning;
-      ResourceVector2f     zoom;
-      ResourceVolumes      volumes;
+      // public input slots
+      ResourceVector3f     position;      /// position in mm of the camera
+      ResourceVector3f     directionx;    /// x-axis of the MPR
+      ResourceVector3f     directiony;    /// y-axis of the MPR
+      ResourceVector3f     panning;       /// panning position in mm
+      ResourceVector2f     zoom;          /// zoom
+      ResourceVolumes      volumes;       /// list of volume to display
 
    public:
       EngineMpr( ResourceStorageVolumes storage, EngineHandler& handler, OrderProvider& provider, OrderDispatcher& dispatcher ) : EngineOrder( handler, provider, dispatcher ), volumes( storage )
