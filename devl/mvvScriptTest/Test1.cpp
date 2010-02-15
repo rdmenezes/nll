fef6f9ad7ffebe7d4e5a4287ba1d5899c97f2791
civodlu <civodlu@355e9548-2d90-11de-ba44-d58fe8a9ce33>
2010-02-15 01:19:30 +0000

    up

--- devl/mvvScriptTest/Test1.cpp
+++ devl/mvvScriptTest/Test1.cpp
@@ -1,6 +1,7 @@
 #include <tester/register.h>
 #include <mvvPlatform/types.h>
 #include <mvvScript/parser-context.h>
+#include <mvvScript/visitor-print.h>
 
 using namespace mvv;
 using namespace mvv::platform;
@@ -37,8 +38,69 @@ struct TestBasic
 
       std::cout << "msg=" << context.getError();
    }
+
+   void testDummy2()
+   {
+       ParserContext context;
+       Ast* exp = 0;
+      
+
+      exp = context.parseString( "if (1) { 2.5; 5; } else { 3; }" );
+      TESTER_ASSERT( exp );
+      exp = context.parseString( "4;5;6;" );
+      TESTER_ASSERT( exp );
+      exp = context.parseString( "if (1 && 1 + 1) { 2.5; } else { 3; }" );
+      TESTER_ASSERT( exp );
+      exp = context.parseString( "if (1 * atest1.val[a[8]+3] ) { 2.5; 5; } else { 3; }" );
+      TESTER_ASSERT( exp );
+      exp = context.parseString( "class test { var a; int n; };" );
+      TESTER_ASSERT( exp );
+      exp = context.parseString( "call(4, 3); call(); call(4);" );
+      TESTER_ASSERT( exp );
+      exp = context.parseString( "int call( int n ){5;5;}" );
+      TESTER_ASSERT( exp );
+      exp = context.parseString( "class Test{ int haha( test a){5;} };" );
+      TESTER_ASSERT( exp );
+      exp = context.parseString( "int test = 0; int main(){ dsf haha; int test; }" );
+      TESTER_ASSERT( exp );
+      exp = context.parseString( "class Test{ int aa; int haha( test a){5;} };" );
+      TESTER_ASSERT( exp );
+      exp = context.parseString( "class Test{ int aa; int haha( test a){5;} float test2; };" );
+      TESTER_ASSERT( exp );
+      exp = context.parseString( "int main(){test.print(\"asf\"); return 0;}" );
+      TESTER_ASSERT( exp );
+      
+      exp = context.parseString( "{var list; list = {5, 4, 3};}" );
+      TESTER_ASSERT( exp );
+      exp = context.parseString( "var list = {3, 4};" );
+      TESTER_ASSERT( exp );
+      exp = context.parseString( "{int a = 3 + test;}" );
+      TESTER_ASSERT( exp );
+      /*
+      exp = context.parseString( "int[] list = {3, 4};" );
+      TESTER_ASSERT( exp );*/
+
+      std::cout << "msg=" << context.getError();
+   }
+
+   void testTmp()
+   {
+       ParserContext context;
+       VisitorPrint visitor( std::cout );
+       Ast* exp = 0;
+      
+
+       exp = context.parseString( "dfkn[] a;" );
+       std::cout << "msg=" << context.getError();
+       TESTER_ASSERT( exp );
+
+       std::cout << std::endl;
+       visitor( *exp );
+   }
 };
 
 TESTER_TEST_SUITE(TestBasic);
-TESTER_TEST(testDummy1);
+//TESTER_TEST(testDummy2);
+//TESTER_TEST(testDummy1);
+TESTER_TEST(testTmp);
 TESTER_TEST_SUITE_END();
