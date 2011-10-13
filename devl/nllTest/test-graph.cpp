ebd6624d1c01fbb573755557d999a682511dc96e
civodlu@gmail.com <civodlu@gmail.com@355e9548-2d90-11de-ba44-d58fe8a9ce33>
2011-10-13 10:55:56 +0000

    up

--- devl/nllTest/test-graph.cpp
+++ devl/nllTest/test-graph.cpp
@@ -30,6 +30,9 @@ namespace core
 
       struct Arc
       {
+         friend GraphImplAdjencyList;
+
+      private:
          Arc( ui32 duid, ui32 toUid ) : detailUid( duid ), toNodeUid( toUid )
          {}
 
@@ -40,11 +43,13 @@ namespace core
 
       struct Node
       {
-         Node( ui32 duid ) : detailUid( duid ), isValid( true )
+         friend GraphImplAdjencyList;
+
+      private:
+         Node( ui32 duid ) : detailUid( duid )
          {}
 
          ui32  detailUid;
-         bool  isValid;
          Arcs  arcs;
       };
       typedef std::vector<Node>     Nodes;
@@ -74,6 +79,118 @@ namespace core
       typedef std::vector<NodeDetail>        NodeDetails;
       typedef std::vector<ArcDetail>         ArcDetails;
 
+      class ConstNodeIterator
+      {
+      public:
+         ConstNodeIterator( GraphImplAdjencyList& graph, ui32 uid ) : _graph( graph ), _it( graph._nodes.begin() + uid )
+         {}
+
+         ConstNodeIterator& operator++()
+         {
+            ++_it;
+            return *this;
+         }
+
+         ConstNodeIterator& operator++(int)
+         {
+            ConstNodeIterator it( *this );
+            ++_it;
+            return it;
+         }
+
+         bool operator!=( const ConstNodeIterator& it ) const
+         {
+            return _it != it._it;
+         }
+
+         const NodeDetail& operator*() const
+         {
+            return _graph._nodeDetails[ _it->detailUid ];
+         }
+
+      private:
+         ConstNodeIterator operator=( ConstNodeIterator& ); // disabled copy
+
+      protected:
+         GraphImplAdjencyList&      _graph;
+         typename Nodes::iterator   _it;
+      };
+
+      class NodeIterator : public ConstNodeIterator
+      {
+      public:
+         NodeIterator( GraphImplAdjencyList& graph, ui32 uid ) : ConstNodeIterator( graph, uid )
+         {}
+
+         bool operator!=( const NodeIterator& it ) const
+         {
+            return _it != it._it;
+         }
+
+         NodeDetail& operator*()
+         {
+            return _graph._nodeDetails[ _it->detailUid ];
+         }
+      };
+
+      class ConstArcIterator
+      {
+      public:
+         ConstArcIterator( GraphImplAdjencyList& graph, ui32 nodeUid, ui32 arcUid ) : _graph( graph ), _it( graph._nodes[ nodeUid ].arcs.begin() + arcUid )
+         {}
+
+         ConstArcIterator& operator++()
+         {
+            ++_it;
+            return *this;
+         }
+
+         ConstArcIterator& operator++(int)
+         {
+            ConstArcIterator it( *this );
+            ++_it;
+            return it;
+         }
+
+         bool operator!=( const ConstArcIterator& it ) const
+         {
+            return _it != it._it;
+         }
+
+         const ArcDetail& operator*() const
+         {
+            return _graph._arcDetails[ _it->detailUid ];
+         }
+
+      private:
+         ConstArcIterator operator=( ConstArcIterator& ); // disabled copy
+
+      protected:
+         GraphImplAdjencyList&      _graph;
+         typename Arcs::iterator    _it;
+      };
+
+      class ArcIterator : public ConstArcIterator
+      {
+      public:
+         ArcIterator( GraphImplAdjencyList& graph, ui32 nodeUid, ui32 arcUid ) : ConstArcIterator( graph, nodeUid, arcUid )
+         {}
+
+         ArcDetail& operator*()
+         {
+            return _graph._arcDetails[ _it->detailUid ];
+         }
+      };
+
+      class Visitor
+      {
+      public:
+         virtual void start(){}
+         virtual void end(){}
+      };
+
+      
+
    public:
       GraphImplAdjencyList()
       {}
@@ -83,36 +200,103 @@ namespace core
       {
          return addNodeImpl( Direction(), detail );
       }
-
       
       ArcDescriptor addArc( const NodeDescriptor& n1, const NodeDescriptor& n2, const ArcDetail& detail )
       {
          return addArcImpl( Direction(), n1, n2, detail );
       }
 
-      Arc getArc( const NodeDescriptor& n1, const NodeDescriptor& n2, bool& found )
+      ArcDescriptor getArc( const NodeDescriptor& n1, const NodeDescriptor& n2, bool& found )
       {
          return getArcImpl( Direction(), n1, n2, found );
+      }
+
+      NodeIterator beginNodes()
+      {
+         return NodeIterator( *this, 0 );
+      }
+
+      NodeIterator endNodes()
+      {
+         return NodeIterator( *this, _nodes.size() );
+      }
+
+      ConstNodeIterator beginNodes() const
+      {
+         return ConstNodeIterator( *this, 0 );
+      }
+
+      ConstNodeIterator endNodes() const
+      {
+         return ConstNodeIterator( *this, _nodes.size() );
+      }
+
+      ArcIterator beginOutEdges( const NodeDescriptor& desc )
+      {
+         return ArcIterator( *this, desc.storageUid, 0 );
+      }
+
+      ConstArcIterator beginOutEdges( const NodeDescriptor& desc ) const
+      {
+         return ConstArcIterator( *this, desc.storageUid, 0 );
+      }
+
+      ArcIterator endOutEdges( const NodeDescriptor& desc )
+      {
+         return ArcIterator( *this, desc.storageUid, _nodes[ desc.storageUid ].arcs.size() );
+      }
+
+      ConstArcIterator endOutEdges( const NodeDescriptor& desc ) const
+      {
+         return ConstArcIterator( *this, desc.storageUid, _nodes[ desc.storageUid ].arcs.size() );
+      }
+
+      const ArcDetail& getArcDetail( const ArcDescriptor& desc ) const
+      {
+         return _arcDetails[ desc.detailUid ];
+      }
+
+      ArcDetail& getArcDetail( const ArcDescriptor& desc )
+      {
+         return _arcDetails[ desc.detailUid ];
+      }
+
+      const ArcDetail& getArcDetail( const Arc& desc ) const
+      {
+         return _arcDetails[ desc.detailUid ];
+      }
+
+      ArcDetail& getArcDetail( const Arc& desc )
+      {
+         return _arcDetails[ desc.detailUid ];
+      }
+
+      NodeDetail& getNodeDetail( const NodeDescriptor& desc )
+      {
+         return _nodeDetails[ desc.detailUid ];
+      }
+
+      const NodeDetail& getNodeDetail( const NodeDescriptor& desc ) const
+      {
+         return _nodeDetails[ desc.detailUid ];
+      }
+
+      NodeDetail& getNodeDetail( const Node& desc )
+      {
+         return _nodeDetails[ desc.detailUid ];
+      }
+
+      const NodeDetail& getNodeDetail( const Node& desc ) const
+      {
+         return _nodeDetails[ desc.detailUid ];
       }
 
    private:
       NodeDescriptor createNode( ui32 detailUid )
       {
-         if ( _recycleNodeDescriptors.size() )
-         {
-            const ui32 uidRecycled = *_recycleNodeDescriptors.rbegin();
-            Node& node = _nodes[ uidRecycled ];
-            _recycleNodeDescriptors.pop_back();
-
-            node.isValid = true;
-            node.arcs.clear();
-            return NodeDescriptor( uidRecycled, detailUid );
-         } else {
-            NodeDescriptor desc( _nodes.size(), detailUid );
-
-            _nodes.push_back( Node( detailUid ) );
-            return desc;
-         }
+         NodeDescriptor desc( _nodes.size(), detailUid );
+         _nodes.push_back( Node( detailUid ) );
+         return desc;
       }
 
       NodeDetail& createNodeDetail( const NodeDetail& detail, ui32& uid )
@@ -121,7 +305,7 @@ namespace core
          {
             const ui32 uidRecycled = *_recycleNodeDetails.rbegin();
             NodeDetail& d = _nodeDetails[ uidRecycled ];
-            _recycleNodeDescriptors.pop_back();
+            _recycleNodeDetails.pop_back();
 
             uid = uidRecycled;
             d = detail;
@@ -161,7 +345,7 @@ namespace core
 
 
       template <class IsDirected>
-      Arc getArcImpl( IsDirected fswitch, const NodeDescriptor& n1, const NodeDescriptor& n2, bool& found );
+      ArcDescriptor getArcImpl( IsDirected fswitch, const NodeDescriptor& n1, const NodeDescriptor& n2, bool& found );
 
       template <>
       NodeDescriptor addNodeImpl( Directed, const NodeDetail& detail )
@@ -190,7 +374,7 @@ namespace core
       }
 
       template <>
-      Arc getArcImpl( Directed, const NodeDescriptor& n1, const NodeDescriptor& n2, bool& found )
+      ArcDescriptor getArcImpl( Directed, const NodeDescriptor& n1, const NodeDescriptor& n2, bool& found )
       {
          Node& node = _nodes[ n1.storageUid ];
          Arcs& arcs = node.arcs;
@@ -198,10 +382,10 @@ namespace core
          if ( it != arcs.end() )
          {
             found = true;
-            return *it;
+            return ArcDescriptor( it - arcs.begin(), it->detailUid, n1, n2 );
          } else {
             found = false;
-            return Arc( (ui32)-1, (ui32)-1 );
+            return ArcDescriptor( (ui32)-1, (ui32)-1, n1, n2 );
          }
       }
 
@@ -227,7 +411,6 @@ namespace core
       ArcDetails        _arcDetails;
       std::vector<ui32> _recycleNodeDetails;
       std::vector<ui32> _recycleArcDetails;
-      std::vector<ui32> _recycleNodeDescriptors;
    };
 
    template <class Impl = GraphImpl<> >
@@ -241,13 +424,16 @@ namespace core
       typedef typename Impl::Node           Node;
       typedef typename Impl::Arc            Arc;
       typedef Impl                          GraphImpl;
-      typedef typename Impl::Nodes::iterator NodeIterator;
+
+      typedef typename Impl::NodeIterator      NodeIterator;
+      typedef typename Impl::ConstNodeIterator ConstNodeIterator;
+      typedef typename Impl::ArcIterator       ArcIterator;
+      typedef typename Impl::ConstArcIterator  ConstArcIterator;
 
    public:
       Graph( const Impl impl = Impl() ) : _impl( impl )
       {}
 
-      
       NodeDescriptor addNode( const NodeDetail details = NodeDetail() )
       {
          return _impl.addNode( details );
@@ -258,17 +444,90 @@ namespace core
          return _impl.addArc( n1, n2, d );
       }
 
-      Arc getArc( const NodeDescriptor& n1, const NodeDescriptor& n2, bool& found )
+      ArcDescriptor getArc( const NodeDescriptor& n1, const NodeDescriptor& n2, bool& found )
       {
          return _impl.getArc( n1, n2, found );
       }
 
-      NodeIterator beginNodes();
+      NodeIterator beginNodes()
+      {
+         return _impl.beginNodes();
+      }
 
+      NodeIterator endNodes()
+      {
+         return _impl.endNodes();
+      }
 
+      ConstNodeIterator beginNodes() const
+      {
+         return _impl.beginNodes();
+      }
 
+      ConstNodeIterator endNodes() const
+      {
+         return _impl.endNodes();
+      }
 
+      ArcIterator beginOutEdges( const NodeDescriptor& desc )
+      {
+         return _impl.beginOutEdges( desc );
+      }
 
+      ArcIterator beginOutEdges( const NodeDescriptor& desc ) const
+      {
+         return _impl.beginOutEdges( desc );
+      }
+
+      ArcIterator endOutEdges( const NodeDescriptor& desc )
+      {
+         return _impl.endOutEdges( desc );
+      }
+
+      ArcIterator endOutEdges( const NodeDescriptor& desc ) const
+      {
+         return _impl.endOutEdges( desc );
+      }
+
+      const ArcDetail& operator[]( const ArcDescriptor& desc ) const
+      {
+         return _impl.getArcDetail( desc );
+      }
+
+      ArcDetail& operator[]( const ArcDescriptor& desc )
+      {
+         return _impl.getArcDetail( desc );
+      }
+
+      const ArcDetail& operator[]( const Arc& desc ) const
+      {
+         return _impl.getArcDetail( desc );
+      }
+
+      ArcDetail& operator[]( const Arc& desc )
+      {
+         return _impl.getArcDetail( desc );
+      }
+
+      const NodeDetail& operator[]( const NodeDescriptor& desc ) const
+      {
+         return _impl.getNodeDetail( desc );
+      }
+
+      NodeDetail& operator[]( const NodeDescriptor& desc )
+      {
+         return _impl.getNodeDetail( desc );
+      }
+
+      const NodeDetail& operator[]( const Node& desc ) const
+      {
+         return _impl.getNodeDetail( desc );
+      }
+
+      NodeDetail& operator[]( const Node& desc )
+      {
+         return _impl.getNodeDetail( desc );
+      }
 
    private:
       GraphImpl   _impl;
@@ -286,13 +545,34 @@ public:
 
       Graph::NodeDescriptor n1 = g.addNode( 10 );
       Graph::NodeDescriptor n2 = g.addNode( 11 );
+      Graph::NodeDescriptor n3 = g.addNode( 12 );
 
       Graph::ArcDescriptor a2 = g.addArc( n1, n2, "test" );
+      Graph::ArcDescriptor a3 = g.addArc( n1, n3, "test2" );
 
       bool found;
-      Graph::Arc a2a = g.getArc( n1, n2, found );
+      Graph::ArcDescriptor a2a = g.getArc( n1, n2, found );
 
 
+      for ( Graph::NodeIterator it = g.beginNodes(); it != g.endNodes(); ++it )
+      {
+         std::cout << "V=" << *it << std::endl;
+      }
+
+      for ( Graph::ConstNodeIterator it = g.beginNodes(); it != g.endNodes(); ++it )
+      {
+         std::cout << "V=" << *it << std::endl;
+      }
+
+      for ( Graph::ArcIterator it = g.beginOutEdges( n1 ); it != g.endOutEdges( n1 ); ++it )
+      {
+         std::cout << "A=" << *it << std::endl;
+      }
+
+      for ( Graph::ConstArcIterator it = g.beginOutEdges( n1 ); it != g.endOutEdges( n1 ); ++it )
+      {
+         std::cout << "A=" << *it << std::endl;
+      }
    }
 };
 
